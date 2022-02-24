2b91aa834b15539647a0ad4e8f922bab52e13fc0
k.Lithinkumar <93367914+Punugulutindam@users.noreply.github.com>
2022-02-24 18:18:11 +0000

    Initial commit

---
+++ chess.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main ()
+{
+   int x,n,y,m,solution;
+   printf("enter the first row and first column");
+   scanf("%d %d ",&x,&n);
+   printf("enter the second row and second column");
+   scanf("%d %d",&y,&m);
+   solution=n+x+m+y;
+   if(solution%2==0)
+   {
+        printf("True");
+    }
+     else
+     {
+            printf("False");
+        }
+            
+            
+    
+}
